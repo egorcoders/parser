43b0ebb967615988e0092f56326f09809e40402c
Egor <egorcoders@gmail.com>
2022-08-15 15:16:17 +0500

    added SQLite db

--- delete.py
+++
@@ -1,5 +0,0 @@
-import datetime as dt
-t = dt.datetime.now()
-
-
-print(t.strftime('%m/%d/%Y, %H:%M'))
--- parser_bot.py
+++ parser_bot.py
@@ -1,89 +1,34 @@
+import datetime as dt
 import logging
 import os
 import re
+import sqlite3
 import time
-import datetime as dt
-t = dt.datetime.now()
 
 import requests
 import telegram
 from bs4 import BeautifulSoup as Bs
 from dotenv import load_dotenv
 
+t = dt.datetime.now()
+
+
 load_dotenv()
 
 
-class ServiceError(Exception):
-    """Ошибка отсутствия доступа по заданному эндпойнту."""
-
-    pass
-
-
-class NetworkError(Exception):
-    """Ошибка отсутствия сети."""
-
-    pass
-
-
-class EndpointError(Exception):
-    """Ошибка, если эндпойнт не корректен."""
-
-    pass
-
-
-class MessageSendingError(Exception):
-    """Ошибка отправки сообщения."""
-
-    pass
-
-
-class GlobalsError(Exception):
-    """Ошибка, если есть пустые глобальные переменные."""
-
-    pass
-
-
-class DataTypeError(Exception):
-    """Ошибка, если тип данных не dict."""
-
-    pass
-
-
-class ResponseFormatError(Exception):
-    """Ошибка, если формат response не json."""
-
-    pass
-
-
-CONNECTION_ERROR = '{error}, {url}, {headers}, {params}'
-SERVICE_REJECTION = '{code}'
-WRONG_ENDPOINT = '{response_status}, {url}, {headers}, {params}'
-WRONG_HOMEWORK_STATUS = '{homework_status}'
-WRONG_DATA_TYPE = 'Неверный тип данных {type}, вместо "dict"'
-STATUS_IS_CHANGED = '{verdict}, {homework}'
-STATUS_IS_NOT_CHANGED = 'Статус не изменился, нет записей'
 FAILURE_TO_SEND_MESSAGE = '{error}, {message}'
 GLOBAL_VARIABLE_IS_MISSING = 'Отсутствует глобальная переменная'
 GLOBAL_VARIABLE_IS_EMPTY = 'Пустая глобальная переменная'
-MESSAGE_IS_SENT = 'Сообщение {message} отправлено'
-FORMAT_NOT_JSON = 'Формат не json {error}'
-LIST_IS_EMPTY = 'Список пустой'
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME_MINUTES = 330
+RETRY_TIME_SECONDS = 60 * 60 * 24
 ENDPOINT = os.getenv('ENDPOINT')
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.',
-}
 
 
 def send_message(bot, message):
-    """Отправляет сообщение пользователю в Телегу."""
+    """Отправляет сообщение пользователю в Телеграм."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
     except Exception as error:
@@ -93,59 +38,6 @@ def send_message(bot, message):
         ))
     logging.info(f'Message "{message}" is sent')
 
-
-# def get_api_answer(current_timestamp):
-#     """Делает запрос к единственному эндпоинту API-сервиса."""
-#     timestamp = current_timestamp or int(time.time())
-#     params = {'from_date': timestamp}
-#     all_params = dict(url=ENDPOINT, headers=HEADERS, params=params)
-#     try:
-#         response = requests.get(**all_params)
-#     except requests.exceptions.RequestException as error:
-#         raise telegram.TelegramError(CONNECTION_ERROR.format(
-#             error=error,
-#             **all_params,
-#         ))
-#     response_status = response.status_code
-#     if response_status != 200:
-#         raise EndpointError(WRONG_ENDPOINT.format(
-#             response_status=response_status,
-#             **all_params,
-#         ))
-#     try:
-#         return response.json()
-#     except Exception as error:
-#         raise ResponseFormatError(FORMAT_NOT_JSON.format(error))
-
-
-# def check_response(response):
-#     """
-#     Возвращает домашку, если есть.
-#     Проверяет валидность её статуса.
-#     """
-#     if 'code' in response:
-#         raise ServiceError(SERVICE_REJECTION.format(
-#             code=response.get('code'),
-#         ))
-#     if response['homeworks']:
-#         return response['homeworks'][0]
-#     else:
-#         raise IndexError(LIST_IS_EMPTY)
-
-
-# def parse_status(homework):
-#     """Возвращает текст сообщения от ревьюера."""
-#     if not isinstance(homework, dict):
-#         raise DataTypeError(WRONG_DATA_TYPE.format(type(homework)))
-#     homework_name = homework.get('homework_name')
-#     homework_status = homework.get('status')
-
-#     if homework_status not in HOMEWORK_STATUSES:
-#         raise NameError(WRONG_HOMEWORK_STATUS.format(homework_status))
-
-#     verdict = HOMEWORK_STATUSES[homework_status]
-
-#     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 def parse_status(current_timestamp):
     """Делает запрос к единственному эндпоинту API-сервиса."""
@@ -161,19 +53,19 @@ def parse_status(current_timestamp):
     for review in reviews:
         review_date = review.select('cat-brand-ugc-date > a')[0].text.strip()
         company_name = review.select('cat-brand-name > a')[1].text.strip()
-        if 'Сегодня' in review_date and company_name != 'Вектор':
+        if 'Сегодня' in review_date and company_name == 'Вкусно — и точка':
             review_format_date = review_date.replace('Сегодня в', timestamp)
             review_url = (
-                'Источник: ' + review.find(
+                review.find(
                     'a', class_='link name t-text t-text--bold'
                 ).get('href')
             )
 
             review_author = (
-                'Автор: ' + review.select('cat-brand-name > a')[0].text.strip()
+                review.select('cat-brand-name > a')[0].text.strip()
             )
 
-            review_rating = 'Рейтинг: ' + review.find(
+            review_rating = review.find(
                 'li', class_='review-estimation__item--checked'
             ).text.strip()
 
@@ -182,20 +74,27 @@ def parse_status(current_timestamp):
             for comment in comments:
                 review_text += ' ' + comment.text.strip()
 
-            review_text = 'Отзыв: ' + re.sub(
+            review_text = re.sub(
                 r'^.*?Показать целиком ', '', review_text
             ).strip().replace('  ', ' ')
 
             personal_info = (
-                review_url + '\n' +
-                review_author + '\n' +
+                'Источник: ' + review_url + '\n' +
+                'Автор: ' + review_author + '\n' +
                 review_format_date + '\n' +
-                review_rating + '\n' +
-                review_text + '\n'
+                'Рейтинг: ' + review_rating + '\n' +
+                'Рейтинг: ' + review_text + '\n'
             )
 
             ans += personal_info + '\n\n'
 
+            add_to_db(
+                review_author,
+                review_url,
+                review_format_date,
+                review_rating,
+                review_text,
+            )
     return ans
 
 
@@ -211,6 +110,41 @@ def check_tokens():
     return True
 
 
+def add_to_db(
+    review_author,
+    review_url,
+    review_format_date,
+    review_rating,
+    review_text,
+):
+    '''Создание БД SQLite'''
+    db = sqlite3.connect('parser.db')
+    c = db.cursor()
+    c = db.execute('''CREATE TABLE IF NOT EXISTS users (
+        author TEXT,
+        url TEXT,
+        date DATETIME,
+        rating INTEGER,
+        text TEXT
+    )''')
+    db.commit()
+
+    c.execute(f"SELECT text FROM users WHERE text = '{review_text}'")
+
+    if c.fetchone() is None:
+        c.execute(
+            'INSERT INTO users VALUES (?, ?, ?, ?, ?)',
+            (
+                review_author,
+                review_url,
+                review_format_date,
+                review_rating,
+                review_text
+            )
+        )
+        db.commit()
+
+
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
@@ -220,12 +154,8 @@ def main():
 
     while True:
         try:
-            # response = get_api_answer(current_timestamp)
-            # homework = check_response(response)
             message = parse_status(current_timestamp)
             send_message(bot, message)
-            # logging.info(homework)
-            # current_timestamp = response.get('current_date')
         except IndexError:
             message = 'Статус работы не изменился'
             send_message(bot, message)
@@ -234,15 +164,8 @@ def main():
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
         finally:
-            time.sleep(RETRY_TIME_MINUTES)
-        # logging.info(MESSAGE_IS_SENT.format(message))
+            time.sleep(RETRY_TIME_SECONDS)
 
 
 if __name__ == '__main__':
-    # logging.basicConfig(
-    #     format='%(asctime)s, %(message)s, %(lineno)d, %(name)s',
-    #     filemode='w',
-    #     filename=f'{Path(__file__).stem}.log',
-    #     level=logging.INFO,
-    # )
     main()
